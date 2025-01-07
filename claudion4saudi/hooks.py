dc42a59d009b55bbf46946b0fa0c224f8b9b8004
Harshafiroz <harsha@htsqatar.com>
2025-01-07 09:56:55 +0300

    Minor changes

--- claudion4saudi/hooks.py
+++ claudion4saudi/hooks.py
@@ -194,9 +194,9 @@ override_whitelisted_methods = {
 # each overriding function accepts a `data` argument;
 # generated from the base implementation of the doctype dashboard,
 # along with any modifications made in other Frappe apps
-override_doctype_dashboards = {
-	"Task": "claudion4saudi.task.get_dashboard_data"
-}
+# override_doctype_dashboards = {
+# 	"Task": "claudion4saudi.task.get_dashboard_data"
+# }
 
 # exempt linked doctypes from being automatically cancelled
 #
