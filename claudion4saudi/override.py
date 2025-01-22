6eb08d781934c7386ca8b64e76ee16a337fa6bd4
Harshafiroz <harsha@htsqatar.com>
2025-01-22 11:06:12 +0300

    Bug fixes

--- claudion4saudi/override.py
+++ claudion4saudi/override.py
@@ -11,7 +11,23 @@ import frappe
 
 @frappe.whitelist()
 def switch_theme(theme):
-    frappe.throw("Theme: " + theme)
-    valid_themes = ["light", "dark", "automatic", "blue", "red", "peach_grey", "purple"]
-    if theme in valid_themes:
-        frappe.db.set_value("User", frappe.session.user, "desk_theme", theme)
+    theme_option = ""
+    if theme == "Light":
+        theme_option = "Frappe Light"
+    if theme == "Dark":
+        theme_option = "Timeless Night"
+    if theme == "Automatic":
+        theme_option = "Automatic"
+    if theme == "Blue":
+        theme_option = "Blue"
+    if theme == "Red":
+        theme_option = "Red"
+    if theme == "Peach_grey":
+        theme_option = "Peach Grey"
+    if theme == "Purple":
+        theme_option = "Purple"
+
+    # frappe.throw("Theme: " + theme)
+    # valid_themes = ["light", "dark", "automatic", "blue", "red", "peach_grey", "purple"]
+    # if theme in valid_themes:
+    frappe.db.set_value("User", frappe.session.user, "desk_theme", theme_option)
